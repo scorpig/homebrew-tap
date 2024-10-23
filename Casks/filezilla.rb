85de0d5abba782ea279e05d4453109ac06bff9d2
scorpig <47138038+scorpig@users.noreply.github.com>
2024-10-23 14:12:57 +0800

    filezilla 3.67.1

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -1,6 +1,6 @@
 cask "filezilla" do
-  version "3.62.2"
-  sha256 "75a683af3338cc7ade56ddf865049c02f985c786b48bbd7a8616466c5cc3e6a6"
+  version "3.67.1"
+  sha256 "e6cf011347d76d0e228cf0ff73c94b317f51775e33bbee76276303cf3ce60b34"
 
   url "https://download.filezilla-project.org/client/FileZilla_#{version}_macosx-x86.app.tar.bz2"
   name "FileZilla"
@@ -9,15 +9,15 @@ cask "filezilla" do
   livecheck do
     skip "No version information available"
   end
-  
+
   depends_on macos: ">= :high_sierra"
 
   app "FileZilla.app"
 
   zap trash: [
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/de.filezilla.sfl*",
-    "~/Library/Saved Application State/de.filezilla.savedState",
-    "~/Library/Preferences/de.filezilla.plist",
     "~/.config/filezilla",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/de.filezilla.sfl*",
+    "~/Library/Preferences/de.filezilla.plist",
+    "~/Library/Saved Application State/de.filezilla.savedState",
   ]
 end
